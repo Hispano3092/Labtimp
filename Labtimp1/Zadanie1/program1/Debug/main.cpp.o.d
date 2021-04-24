6fe90f3bdf22a594a472952789ad90b1cc9623f7
Hispano3092 <81823116+Hispano3092@users.noreply.github.com>
2021-04-24 12:55:00 +0300

    Add files via upload

---
+++ Labtimp1/Zadanie1/program1/Debug/main.cpp.o.d
@@ -0,0 +1,4 @@
+Debug/main.cpp.o: main.cpp \
+ /home/student/my_cplusplus_programs/Labtimp/Labtimp1/Zadanie1/program1/modAlphaCipher.h
+
+/home/student/my_cplusplus_programs/Labtimp/Labtimp1/Zadanie1/program1/modAlphaCipher.h:
---
+++ Labtimp1/Zadanie2/program2/Debug/main.cpp.o.d
@@ -0,0 +1,4 @@
+Debug/main.cpp.o: main.cpp \
+ /home/student/my_cplusplus_programs/Labtimp/Labtimp1/Zadanie2/program2/chifr.h
+
+/home/student/my_cplusplus_programs/Labtimp/Labtimp1/Zadanie2/program2/chifr.h:
